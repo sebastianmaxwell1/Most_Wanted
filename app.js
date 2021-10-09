f1a5f4965b0381f49d5634784bdb87568ab58ab7
Sebastian Maxwell <sebastianrmaxwell@gmail.com>
2021-10-08 21:19:42 -0400

    Created small first/last name search function. Infinite loops though

--- app.js
+++ app.js
@@ -1,106 +1,73 @@
 "use strict"
-/*
-Build all of your functions for displaying and gathering information below (GUI).
-*/
 
-// app is the function called to start the entire application
-function app(people){
-  let searchType = promptFor("Do you know the name of the person you are looking for? Enter 'yes' or 'no'", yesNo).toLowerCase();
-  let searchResults;
-  switch(searchType){
-    case 'yes':
-      searchResults = searchByName(people);
-      break;
-    case 'no':
-      // TODO: search by traits
-      break;
-      default:
-    app(people); // restart app
-      break;
-  }
-  
-  // Call the mainMenu function ONLY after you find the SINGLE person you are looking for
-  mainMenu(searchResults, people);
-}
+let firstNameInput = document.forms['nameForm']['fname'].value;
+let lastNameInput = document.forms['nameForm']['lname'].value;
 
-// Menu function to call once you find who you are looking for
-function mainMenu(person, people){
+// Name Search
+function searchByFirstName(firstNameArray){
+    // Grabbing the values from our nameForm form and inputs.
+    firstNameInput = document.forms['nameForm']['fname'].value;
 
-  /* Here we pass in the entire person object that we found in our search, as well as the entire original dataset of people. We need people in order to find descendants and other information that the user may want. */
+    // "people" is coming from the data.js file. We have access to it within this JavaScript file.
+    let filteredFirstName = firstNameArray.filter(function (person) {
+        if(person.firstName === firstNameInput){
+            return true;
+        }
+        return false;
+    });
 
-  if(!person){
-    alert("Could not find that individual.");
-    return app(people); // restart
-  }
+    // Rather than console logging, you need to append the filteredPeople to a table.
+    if(filteredFirstName.length > 0){
+        console.log(filteredFirstName);
 
-  let displayOption = prompt("Found " + person.firstName + " " + person.lastName + " . Do you want to know their 'info', 'family', or 'descendants'? Type the option you want or 'restart' or 'quit'");
-
-  switch(displayOption){
-    case "info":
-    // TODO: get person's info
-    break;
-    case "family":
-    // TODO: get person's family
-    break;
-    case "descendants":
-    // TODO: get person's descendants
-    break;
-    case "restart":
-    app(people); // restart
-    break;
-    case "quit":
-    return; // stop execution
-    default:
-    return mainMenu(person, people); // ask again
-  }
-}
-
-function searchByName(people){
-  let firstName = promptFor("What is the person's first name?", chars);
-  let lastName = promptFor("What is the person's last name?", chars);
-
-  let foundPerson = people.filter(function(person){
-    if(person.firstName === firstName && person.lastName === lastName){
-      return true;
+    }else{
+        alert('Sorry, looks like there is no one with that First Name.');
     }
-    else{
-      return false;
+    return filteredFirstName;
+}
+
+// Name Search
+function searchByLastName(lastNameArray){
+    // Grabbing the values from  nameForm form and inputs.
+    lastNameInput = document.forms['nameForm']['lname'].value;
+
+    let filteredLastName = lastNameArray.filter(function (person) {
+        if(person.lastName === lastNameInput){
+            return true;
+        }
+        return false;
+    });
+
+    // Rather than console logging, you need to append the filteredPeople to a table.
+    if(filteredLastName.length > 0){
+        console.log(filteredLastName);
+
+    }else{
+        alert('Sorry there is no one with that Last Name');
     }
-  })
-  // TODO: find the person using the name they entered
-  return foundPerson;
+    return filteredLastName;
 }
 
-// alerts a list of people
-function displayPeople(people){
-  alert(people.map(function(person){
-    return person.firstName + " " + person.lastName;
-  }).join("\n"));
-}
 
-function displayPerson(person){
-  // print all of the information about a person:
-  // height, weight, age, name, occupation, eye color.
-  let personInfo = "First Name: " + person.firstName + "\n";
-  personInfo += "Last Name: " + person.lastName + "\n";
-  // TODO: finish getting the rest of the information to display
-  alert(personInfo);
-}
+// main search function
 
-// function that prompts and validates user input
-function promptFor(question, valid){
-  do{
-    var response = prompt(question).trim();
-  } while(!response || !valid(response));
-  return response;
-}
+function mainSearchFunction(){
+    firstNameInput = document.forms['nameForm']['fname'].value;
+    lastNameInput = document.forms['nameForm']['lname'].value;
+    
 
-// helper function to pass into promptFor to validate yes/no answers
-function yesNo(input){
-  return input.toLowerCase() == "yes" || input.toLowerCase() == "no";
-}
+    let searchResults = people;
 
-// helper function to pass in as default promptFor validation
-function chars(input){
-  return true; // default validation only
-}
+    if(firstNameInput.length > 1){
+      searchResults = searchByFirstName(searchResults);
+
+    }
+
+    if(lastNameInput.length > 1){
+      searchResults = searchByLastName(searchResults);
+
+    }
+
+    console.log(searchResults);
+    mainSearchFunction(searchResults);
+};
